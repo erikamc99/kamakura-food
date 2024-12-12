a1f98b0804edc1145a11a585e693edb2f7116a6b
LannyRivero <lannyrivero288@gmail.com>
2024-12-12 17:01:52 +0100

    Step 1

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,14 @@
+import {products} from '../assets/data/data.js'
 //DEBE imprimir en pantalla la información de filtros.
 
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
+
+
+
+document.getElementById("products").innerHTML = products.map(getFullProducts);
+
+function getFullProducts(item) {
+    return [item.name, item.description, item.price].join(" ");
+}
+
+
