31ad64c03f0ba895574db686787b09ad919e8a15
unknown <celiagarridoherrera@gmail.com>
2024-12-13 14:40:51 +0100

    dev

--- src/events.js
+++ src/events.js
@@ -1,2 +1,11 @@
 //Intenta separar los eventos en este archivo.
 
+import {products} from '../assets/data/data.js'
+import { getFullProducts } from './menu.js';
+
+function init() {    
+
+    getFullProducts(products);
+}
+
+init();
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,25 @@
-import {filters, products} from "../assets/data/data.js"
+import { products } from '../assets/data/data.js';
+
+const productContainer = document.getElementById("products");
+productContainer.innerHTML = products.map(getFullProducts).join("");
+
+export function getFullProducts(item) {
+    return `
+        <div class="product-container">
+            <h3>${item.name}</h3>
+            <p>${item.description}</p>
+            <div class="price-container">
+                <h5>${item.price}€</h5>
+                <button class="add-button">Añadir</button>
+            </div>
+        </div>
+    `;
+}
+
+
+
+
+import {filters} from "../assets/data/data.js"
 
 //DEBE imprimir en pantalla la información de filtros.
 
