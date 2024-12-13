20d4b933860b1413d20c9003640fffbec7d4041d
LannyRivero <lannyrivero288@gmail.com>
2024-12-13 14:09:24 +0100

    create products

--- src/menu.js
+++ src/menu.js
@@ -1,14 +1,20 @@
-import {products} from '../assets/data/data.js'
-//DEBE imprimir en pantalla la información de filtros.
+import { products } from '../assets/data/data.js';
 
-//DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
+const productContainer = document.getElementById("products");
+productContainer.innerHTML = products.map(getFullProducts).join("");
 
-
-
-document.getElementById("products").innerHTML = products.map(getFullProducts);
-
-function getFullProducts(item) {
-    return [item.name, item.description, item.price].join(" ");
+export function getFullProducts(item) {
+    return `
+        <div class="product-container">
+            <h3>${item.name}</h3>
+            <p>${item.description}</p>
+            <div class="price-container">
+                <h5>${item.price}€</h5>
+                <button class="add-button">Añadir</button>
+            </div>
+        </div>
+    `;
 }
+
 
 
