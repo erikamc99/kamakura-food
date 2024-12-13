f5545f280da5d0cb574e974a68c68076f1161558
erikamc99 <erikamc99@gmail.com>
2024-12-13 09:23:56 +0100

    funcion mostrar filtros

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,34 @@
+import {filters, products} from "../assets/data/data.js"
+
 //DEBE imprimir en pantalla la información de filtros.
+
+/* 
+PRIMERA PRUEBA (Sale pero horrible)
+let newArray = filters.map(myFilters);
+
+document.getElementById("filters").innerHTML = newArray;
+
+function myFilters(filter) {
+    return `${filter}`;
+} 
+*/
+    
+    // Selecciona el botón 
+    let filterButton = document.querySelector('.filter');
+    
+    //Poner bonito el texto
+    let capitalizedFilters = filters.map(filter => filter.charAt(0).toUpperCase() + filter.slice(1)); // Poner la primera en mayúscula :)
+    filterButton.textContent = capitalizedFilters[0]; // Para que se muestre el texto 'Todos' en lugar de Filtros
+    
+    // Evento para que al hacer click que se muestren los filtros
+    filterButton.addEventListener('click', () => {
+    
+        let showFilters = capitalizedFilters.map(filter => `${filter} `).join('');
+        // Para que se muestren en formato lista sería `<li>${filter}</li>`
+
+        
+    // Actualizar el contenido del HTML
+    document.getElementById('filters').innerHTML = `${showFilters}`;
+});
 
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
